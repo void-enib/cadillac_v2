c9e88699ac41c0afa187013719f074578c50e1fe
Bastien <47535482+Arcadeuh@users.noreply.github.com>
2019-11-07 23:06:14 +0100

    Commit Gun : Ajout asset gun, avec visée possible

---
+++ Assets/Scripts/gunMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pick the mouse's position in the scene, considering the transform as the origin
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+        //angle between the x axis and the line which pass trough origin and diff, in degree
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        //set rotation
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+}
