bab59a3672cf1eeb9058c043018be48de1127a3e
quentin V <q8vigner@enib.fr>
2019-11-21 15:35:40 +0100

    Ajout de Balle et de son mouvement

---
+++ Assets/move_balle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class move_balle : MonoBehaviour
+{
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0.0f, speed, 0.0f);
+
+    }
+}
