5e34d231865218816fb3d373da5913e9b9eacf41
Bastien <47535482+Arcadeuh@users.noreply.github.com>
2019-11-21 16:28:01 +0100

    Life bar

---
+++ Assets/Scripts/lifeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lifeManager : MonoBehaviour
+{
+    public int life = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(life==0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet")
+        {
+            life--;
+            Destroy(collision.gameObject);
+            Debug.Log(life);
+        }
+    }
+
+}
