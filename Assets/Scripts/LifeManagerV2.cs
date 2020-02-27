c1a408ed899e2198ea0e5a3bc1f6947b611781b5
Arcadeuh <47535482+Arcadeuh@users.noreply.github.com>
2020-02-27 16:52:08 +0100

    Collision voiture balle refaite

---
+++ Assets/Scripts/LifeManagerV2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeManagerV2 : MonoBehaviour
+{
+
+    [SerializeField] private int life = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        life -= 1;
+    }
+
+    private void Update()
+    {
+        Debug.Log(GetComponent<Rigidbody2D>().velocity);
+        if(life<=0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
