e2ed723f867b4cbc3177f8abd4c60aef77419e34
quentin V <q8vigner@enib.fr>
2020-03-08 09:41:52 +0100

    Début de création des tires

---
+++ Assets/Scripts/Balle_avance.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balle_avance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public Transform Direction1;
+    Vector2 Direction;
+    public int Xmin;
+    public int Xmax;
+    public int Ymin;
+    public int Ymax;
+    int a;
+    void Start()
+    {
+        //GetComponent<Rigidbody2D>().velocity = transform.forward * speed;
+        a = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Direction.x = Direction1.position.x;
+        Direction.y= Direction1.position.y;
+        Debug.Log(transform.position);
+        transform.Translate(Direction.normalized * speed * Time.deltaTime);
+        //if (transform.position.x == 0)
+        //{ }
+        //else if (((transform.position.x < Xmin) || (transform.position.x > Xmax)) && Time.time > 1)
+        //{ Destroy(gameObject); }
+        //else if (((transform.position.y < Ymin) || (transform.position.y > Ymax)) && Time.time > 1)
+        //{ Destroy(gameObject); }
+        a = 0;
+    }
+}
--- Assets/Scripts/Tire_1.cs
+++ Assets/Scripts/Tire_1.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Tire_1 : MonoBehaviour
 {
+    //public Vector2 balle;
     // Start is called before the first frame update
+    //objet statique public Instantiate ( objet original , position Vector3 , rotation du Quaternion );
+    public GameObject balle;
+    public Transform positionVoiture;
+    public Transform positionCannon;
+    private Vector3 positionBalle;
+    private Quaternion rotationBalle;
+    public float nextFire;
+    public float speed;
     void Start()
     {
-       public Vector2 balle;
-}
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log (Input.GetButton("Fire1"));
+
+        Quaternion quaternionAngles = transform.rotation;
+        Vector3 eulerAngles = quaternionAngles.eulerAngles;
+        eulerAngles.z = eulerAngles.z - 270;
+        //Debug.Log(eulerAngles);
+        eulerAngles = ((eulerAngles *(Mathf.PI) )/ 180);
+        positionBalle.x = Mathf.Cos(eulerAngles.z)*1.8f +transform.position.x;
+        positionBalle.y = Mathf.Sin(eulerAngles.z)*1.8f+ transform.position.y;
+
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+            {
+            Instantiate(balle, new Vector3(positionBalle.x, positionBalle.y, 0), Quaternion.identity);
+            
+        }
+        //gameObject.transform.Translate(Direction.normalized * speed * Time.deltaTime);
+
+
     }
 }
